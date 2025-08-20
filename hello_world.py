e27a55fbd22e8bfc3063390449e3667ed1e42822
Frank J Schwab <frank@frankschwab.de>
2025-08-20 15:30:06 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+# hello_thonny.py
+# Dein erstes Python-Programm in Thonny 🚀
+
+# Hallo Welt ausgeben
+print("Hello World from Thonny!")
+
+# Einfache Rechnung
+zahl1 = 7
+zahl2 = 5
+ergebnis = zahl1 + zahl2
+print("7 + 5 =", ergebnis)
+
+# Benutzereingabe
+name = input("Wie heißt du? ")
+print("Hallo,", name, "😊 willkommen in Python!")
